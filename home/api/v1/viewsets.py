33c9238db2a057041d596aafe1a274dfdeea2ab8
crowdbotics-apps <team@crowdbotics.com>
2023-01-25 05:36:27 +0000

    Update models

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Sales
+from .serializers import SalesSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class SalesViewSet(viewsets.ModelViewSet):
+    serializer_class = SalesSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Sales.objects.all()
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Sales(models.Model):
+    'Generated Model'
+    salesDate = models.DateTimeField(auto_now=True,)
+    qty = models.SmallIntegerField()
+    price = models.BigIntegerField()
