e614accaa85b80030c50fb34b30d2f64d05a9df5
crowdbotics-apps <team@crowdbotics.com>
2023-01-25 11:32:31 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -8,3 +8,4 @@ class Order(models.Model):
     'Generated Model'
     qty = models.SmallIntegerField()
     orderDate = models.DateTimeField(null=True,blank=True,)
+    item = models.OneToOneField("home.Sales",on_delete=models.CASCADE,null=True,blank=True,related_name="order_item",)
